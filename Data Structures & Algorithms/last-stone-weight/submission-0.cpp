e97825c9eaaf5cb879863b9c20bac59c0baf9be0
priyansh3010 <pri.obhan30@gmail.com>
2026-05-20 14:32:37 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+
+        for (int stone : stones) maxHeap.push(stone);
+
+        while (maxHeap.size() > 1) {
+            int stone1 = maxHeap.top(); maxHeap.pop();
+            int stone2 = maxHeap.top(); maxHeap.pop();
+
+            if (stone1 == stone2) continue;
+            maxHeap.push(stone1 - stone2);
+        }
+
+        return maxHeap.empty() ? 0 : maxHeap.top();
+    }
+};
