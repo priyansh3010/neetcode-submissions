5977ec6677a82c86945ba2428d4ed1e9eca439e1
priyansh3010 <pri.obhan30@gmail.com>
2026-07-23 14:23:13 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp(coins.size(), vector<int>(amount, -1));
+
+        return dfs(dp, coins, amount, 0, 0);
+    }
+
+    int dfs(vector<vector<int>>& dp, vector<int>& coins, int& target, int curr, int i) {
+        if (i >= coins.size()) return 0;
+        if (curr >= target) return curr == target ? 1 : 0;
+
+        if (dp[i][curr] >= 0) return dp[i][curr];
+
+        return dp[i][curr] = dfs(dp, coins, target, curr, i + 1) +
+                             dfs(dp, coins, target, curr + coins[i], i);
+    }
+};
