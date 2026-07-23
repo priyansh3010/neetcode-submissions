95d40416971508cd4bc673b4c7e238180ca6d743
priyansh3010 <pri.obhan30@gmail.com>
2026-07-23 15:59:45 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        vector<unordered_map<int, int>> dp(nums.size(), unordered_map<int, int>());
+
+        return dfs(dp, nums, target, 0, 0);
+    }
+
+    int dfs(vector<unordered_map<int, int>>& dp, vector<int>& nums, int& target, int curr, int i) {
+        if (i >= nums.size()) return curr == target ? 1 : 0;
+
+        dp[i][curr] = dfs(dp, nums, target, curr + nums[i], i + 1) +
+                      dfs(dp, nums, target, curr - nums[i], i + 1);
+        
+        return dp[i][curr]; 
+    }
+};
