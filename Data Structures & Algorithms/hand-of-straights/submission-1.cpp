8ad82c703d04454f470b4ae6748f357df9f94825
priyansh3010 <pri.obhan30@gmail.com>
2026-07-27 16:35:53 +0530

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) return false;
+
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        unordered_map<int, int> freq;
+
+        for (int card : hand) {
+            if (freq.find(card) == freq.end()) minHeap.push(card);
+            freq[card]++;
+        }
+
+        while (!minHeap.empty()) {
+            int top = minHeap.top();
+            for (int i = top; i < top + groupSize; i++) {
+                if (freq.find(i) == freq.end()) {
+                    return false;
+                } 
+                freq[i]--;
+                if (freq[i] == 0) {
+                    if (i == minHeap.top()) minHeap.pop();
+                    else return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
