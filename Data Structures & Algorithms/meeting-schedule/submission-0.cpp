0fe757793754bd3fc667b8658d8ab1cfd5dcb3db
priyansh3010 <pri.obhan30@gmail.com>
2026-08-03 17:41:07 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](Interval& a, Interval& b) {
+            if (a.start != b.start) return a.start < b.start;
+            return a.end < b.end;
+        });
+
+        Interval prev = intervals[0];
+        for (int i = 1; i < intervals.size(); i++) {
+            cout << prev.start << " " << prev.end << endl;
+            if (prev.end > intervals[i].start) return false;
+            prev = intervals[i];
+        }
+
+        return true;
+    }
+};
